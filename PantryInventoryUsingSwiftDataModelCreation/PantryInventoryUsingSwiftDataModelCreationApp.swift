d3c4c75b0f04c46604430669bfe31d9b30385638
ribragimov-mwb <ribragimov@malwarebytes.com>
2024-05-10 20:18:09 +0300

    Initial Commit

---
+++ PantryInventoryUsingSwiftDataModelCreation/PantryInventoryUsingSwiftDataModelCreationApp.swift
@@ -0,0 +1,17 @@
+//
+//  PantryInventoryUsingSwiftDataModelCreationApp.swift
+//  PantryInventoryUsingSwiftDataModelCreation
+//
+//  Created by Ramill Ibragimov on 5/10/24.
+//
+
+import SwiftUI
+
+@main
+struct PantryInventoryUsingSwiftDataModelCreationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
